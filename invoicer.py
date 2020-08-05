0ebc03b4e47cfd2db35910bf02200db7132a4427
Hamuj12 <69257879+Hamuj12@users.noreply.github.com>
2020-08-05 15:16:41 -0500

    Update invoicer.py

--- invoicer.py
+++ invoicer.py
@@ -13,27 +13,50 @@ def getRawText(pdfFile):
     pdfFileObj.close()
     return text
 
-rawText = getRawText('BelBrands1.pdf')
-print(rawText)
+def findInvoiceAndDueDates(rawText):
+    #Invoice Date and Due Date Regexes
+    invoiceANDdateRegex = re.compile(r'Invoice ?Date\n([0-9/]+)', re.IGNORECASE)
+    dueDateRegex = re.compile(r'(Due ?Date)|(TotalAmountDueon)', re.IGNORECASE)
 
-#Invoice Date Regexes
-invoiceANDdateRegex = re.compile(r'Invoice ?Date\n([0-9/]+)', re.IGNORECASE)
-dueDateRegex = re.compile(r'Due ?Date')
+    invoiceRegex = re.compile(r'Invoice ?Date', re.IGNORECASE)
+    invoiceMO = invoiceRegex.findall(rawText)
 
-invoiceRegex = re.compile(r'Invoice ?Date', re.IGNORECASE)
-invoiceMO = invoiceRegex.findall(rawText)
+    dateRegex = re.compile(r'\d\d?/\d\d?/\d\d?')
+    dateMO = dateRegex.findall(rawText)
 
-dateRegex = re.compile(r'\d\d?/\d\d?/\d\d?')
-dateMO = dateRegex.findall(rawText)
+    #Finding the Invoice Date(s)
+    if invoiceANDdateRegex.findall(rawText):
+        invoiceDateMO = invoiceANDdateRegex.findall(rawText)
+        print('Invoice Date: ' + invoiceDateMO[0])
+    elif len(dateMO) >= 2 and invoiceMO:
+        print('Invoice Date: ' + dateMO[0])
+    else:
+        print('Invoice Date not found!')
 
-#Finding the Invoice and Due Dates
-if invoiceANDdateRegex.findall(rawText):
-    invoiceDateMO = invoiceANDdateRegex.findall(rawText)
-    print('Invoice Date: ' + invoiceDateMO[0])
-elif len(dateMO) >= 2 and invoiceMO:
-    print('Invoice Date: ' + dateMO[0])
-else:
-    print('Invoice Date not found!')
+    #Finding the Due Date(s)
+    if dueDateRegex.findall(rawText) and len(dateMO) >= 2:
+        print('Due Date: ' + dateMO[1])
+    else:
+        print('Due date not found!')
+
+#Test Cases
+text1 = getRawText('BelBrands1.pdf')
+findInvoiceAndDueDates(text1)
+
+text2 = getRawText('BelBrands2.pdf')
+findInvoiceAndDueDates(text2)
+
+text3 = getRawText('BelBrands3.pdf')
+findInvoiceAndDueDates(text3)
+
+text4 = getRawText('BelBrands4.pdf')
+findInvoiceAndDueDates(text4)
+
+text5 = getRawText('BelBrands5.pdf')
+findInvoiceAndDueDates(text5)
+
+text6 = getRawText('invoice.pdf')
+findInvoiceAndDueDates(text6)
 
 # invoiceNumberRegex = re.compile(r'InvoiceNumber\n(\d+)')
 # mo = invoiceNumberRegex.findall(text)
