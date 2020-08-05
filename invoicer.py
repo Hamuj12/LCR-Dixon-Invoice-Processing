f6670905a526f804cee3cd7a44c706041521a08c
Hamuj12 <69257879+Hamuj12@users.noreply.github.com>
2020-08-05 17:52:19 -0500

    Revert "Revert "Add support fro account numbers""
    This reverts commit f343b3866dedf705ad277559c2eabe62c6e1ae0b.

--- invoicer.py
+++ invoicer.py
@@ -24,7 +24,7 @@ def findInvoiceAndDueDates(rawText):
     dateRegex = re.compile(r'\d\d?/\d\d?/\d\d?')
     dateMO = dateRegex.findall(rawText)
 
-    #Finding the Invoice Date(s)
+    #Find the Invoice Date(s)
     if invoiceANDdateRegex.findall(rawText):
         invoiceDateMO = invoiceANDdateRegex.findall(rawText)
         print('Invoice Date: ' + invoiceDateMO[0])
@@ -33,39 +33,50 @@ def findInvoiceAndDueDates(rawText):
     else:
         print('Invoice Date not found!')
 
-    #Finding the Due Date(s)
+    #Find the Due Date(s)
     if dueDateRegex.findall(rawText) and len(dateMO) >= 2:
         print('Due Date: ' + dateMO[1])
     else:
         print('Due date not found!')
 
+#Find Account Number and Invoice Number
+def findAccountNumber(rawText):
+    #Account Number Regexes
+    fullAccountNumberRegex = re.compile(r'AccountNumber\n(\d+)', re.IGNORECASE)
+    altAccountNumberRegex = re.compile(r'(\w+ )?Account ?Number', re.IGNORECASE)
+    accountNumberRegex = re.compile(r'\d+\n')
+
+    #Find the Account Number
+    if fullAccountNumberRegex.findall(rawText):
+        accountNumberMO = fullAccountNumberRegex.findall(rawText)
+        print('Account Number: ' + accountNumberMO[0])
+    elif altAccountNumberRegex.findall(rawText):
+        print('Account Number Found')
+
+    else:
+        print('Account Number not found!')
+
 #Test Cases
 text1 = getRawText('BelBrands1.pdf')
 findInvoiceAndDueDates(text1)
+findAccountNumber(text1)
 
 text2 = getRawText('BelBrands2.pdf')
 findInvoiceAndDueDates(text2)
+findAccountNumber(text2)
 
 text3 = getRawText('BelBrands3.pdf')
 findInvoiceAndDueDates(text3)
+findAccountNumber(text3)
 
 text4 = getRawText('BelBrands4.pdf')
 findInvoiceAndDueDates(text4)
+findAccountNumber(text4)
 
 text5 = getRawText('BelBrands5.pdf')
 findInvoiceAndDueDates(text5)
+findAccountNumber(text5)
 
 text6 = getRawText('invoice.pdf')
 findInvoiceAndDueDates(text6)
-
-# invoiceNumberRegex = re.compile(r'InvoiceNumber\n(\d+)')
-# mo = invoiceNumberRegex.findall(text)
-# print('Invoice Number: ' + mo[0])
-#
-# accountNumberRegex = re.compile(r'AccountNumber\n(\d+)')
-# mo = accountNumberRegex.findall(text)
-# print('Account Number: ' + mo[0])
-#
-# billingNumberRegex = re.compile(r'BillingNumber\n(\d+)')
-# mo = billingNumberRegex.findall(text)
-# print('Billing Number: ' + mo[0])
+findAccountNumber(text6)
